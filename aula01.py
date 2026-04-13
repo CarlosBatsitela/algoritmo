0699f1a4c45a96f95cfd220f8aab5016c963ca81
CARLOS EDUARDO BATISTELA BARBIERI <carlos.barbieri@univille.br>
2026-04-13 20:24:31 -0300

    algoritmo

---
+++ aula01.py
@@ -0,0 +1,25 @@
+nome=input('Qual seu nome? ')
+print(f'Olá {nome}, seja bem vindo(a) ')
+
+Valor=int(input('Qual valor você quer dobrar? '))
+print(f'O dobro de {Valor} é {Valor*2}')
+
+Valor1=float(input('digite um numero:') )
+Valor2=float(input('digite mais um numero:'))
+print(f'{Valor1} mais {Valor2} é igual á {Valor1+Valor2}')
+
+ano=int(input('Em que ano você nasceu? '))
+print(f'Sua idade é {2026-ano} anos')
+
+salario=float(input('Qual o seu salário? '))
+print(f'Com um aumento de 10% seu salário agora é {salario*0.10}R$')
+Temperatura=float(input('Qual a temperatura em Celsius? '))
+print(f'{Temperatura}ºc em fahrenheit é {Temperatura*9/5+32}ºF')
+
+nome=input('Qual seu nome Completo? ')
+idade=int(input('Qual sua idade? '))
+cidade=input('Qual cidade você mora? ')
+print(f'Nome: {nome}')
+print(f'idade: {idade} anos')
+print(f'Cidade: {cidade}')
+
