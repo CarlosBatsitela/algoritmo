5fae917f735777e25370e328b2dfbbe008b8b9f8
141206Ca@ <carlosebb1412@gmail.com>
2026-04-27 22:07:09 -0300

    cond.

--- condicionais.py
+++ condicionais.py
@@ -1,1 +1,16 @@
+numero=float(input('Digite um numero: '))
 
+if numero >0:
+    print('O numero é positivo')
+elif numero <0:
+    print('O numero é negativo')
+else:
+    print('O numero é zero')
+
+
+numero1=float(input('Escolha um numero: '))
+
+if numero1 % 2 ==0:
+    print('O numero é par')
+else: numero1 % 2 !=0:
+    print('O numero é impar')
