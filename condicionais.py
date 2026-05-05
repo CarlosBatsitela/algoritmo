e1ff17e42ab7f8768fc2b39c2467e104a2d5e719
CARLOS EDUARDO BATISTELA BARBIERI <carlos.barbieri@univille.br>
2026-05-05 20:56:32 -0300

    1

--- condicionais.py
+++ condicionais.py
@@ -6,14 +6,15 @@ elif numero <0:
     print('O numero é negativo')
 else:
     print('O numero é zero')
-
+#1
 
 numero1=float(input('Escolha um numero: '))
 
 if numero1 % 2 ==0:
     print('O numero é par')
-else: numero1 % 2 !=0
+else:
     print('O numero é impar')
+#2
 
 numero2=float(input('digite um numero: '))
 numero3=float(input('digite mais um numero: '))
@@ -22,7 +23,7 @@ if numero2 >numero3:
     print('o maior é {numero2}')
 else:
     print('o maior é {numero3}')
-
+#3,4
 
 nota1=float(input('Digite a primeira nota: '))
 nota2=float(input('Digite a segunda nota: '))
@@ -31,7 +32,7 @@ if media >=7:
     print('Aprovado')
 else:
     print('Reprovado')
-
+#5
 
 idade=int(input('Digite sua idade: '))
 if idade <=18:
@@ -40,12 +41,20 @@ elif idade <=60:
     print('Você é Adulto') 
 else:   
     print('Você é Idoso')
-
+#6
 
 salario=float(input('Digite seu salário: '))
 if salario >5000:
     print('{salario}*0.9')
 else:
     print('{salario}*0.95')
+#7
 
+login=input('Digite seu login: ')
+senha=input('Digite sua senha: ')
+if login == 'admin' and senha == '1234':
+    print('Acesso permitido')
+else:
+    print('Acesso negado')
+#8
 
