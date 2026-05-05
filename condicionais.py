974ace7fd977be3f72aadf34bffb5f51b4f4168a
CarlosBatsitela <carlosebb1412@gmail.com>
2026-05-04 21:53:18 -0300

    Fix conditional logic and add number comparisons

--- condicionais.py
+++ condicionais.py
@@ -14,3 +14,11 @@ if numero1 % 2 ==0:
     print('O numero é par')
 else: numero1 % 2 !=0:
     print('O numero é impar')
+
+numero2=float(input('digite um numero: '))
+numero3=float(input('digite mais um numero: '))
+numero4=float(input)
+if numero2 >numero3:
+    print('o maior é {numero2}')
+else:
+    print('o maior é {numero3}')
