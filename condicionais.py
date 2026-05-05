a71a349fa3bae3397f51dcc2a391803eb51bba29
CARLOS EDUARDO BATISTELA BARBIERI <carlos.barbieri@univille.br>
2026-05-05 20:21:44 -0300

    1

--- condicionais.py
+++ condicionais.py
@@ -12,13 +12,40 @@ numero1=float(input('Escolha um numero: '))
 
 if numero1 % 2 ==0:
     print('O numero é par')
-else: numero1 % 2 !=0:
+else: numero1 % 2 !=0
     print('O numero é impar')
 
 numero2=float(input('digite um numero: '))
 numero3=float(input('digite mais um numero: '))
-numero4=float(input)
+numero4=float(input('digite o ultimo numero: '))
 if numero2 >numero3:
     print('o maior é {numero2}')
 else:
     print('o maior é {numero3}')
+
+
+nota1=float(input('Digite a primeira nota: '))
+nota2=float(input('Digite a segunda nota: '))
+media=({nota1}+{nota2})/2
+if media >=7:
+    print('Aprovado')
+else:
+    print('Reprovado')
+
+
+idade=int(input('Digite sua idade: '))
+if idade <=18:
+    print('Você é Criança')
+elif idade <=60:
+    print('Você é Adulto') 
+else:   
+    print('Você é Idoso')
+
+
+salario=float(input('Digite seu salário: '))
+if salario >5000:
+    print('{salario}*0.9')
+else:
+    print('{salario}*0.95')
+
+
