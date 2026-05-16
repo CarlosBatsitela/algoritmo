77b46f6379594fca4a2cc75bd77c9f270dbd4161
CARLOS EDUARDO BATISTELA BARBIERI <carlos.barbieri@univille.br>
2026-05-15 22:15:33 -0300

    1

--- Repetição.py
+++ Repetição.py
@@ -1,2 +1,23 @@
-for
-while
+'''#1
+t=1
+while t<11:
+    print(t)
+    t+=1
+
+#2
+for t in range(2,21,2):
+    print(t)
+
+#3
+total=0
+for t in range(1,101):
+    total+=t
+    print(total)'''
+
+#4
+soma=0
+quantiadade=5
+for t in range(quantiadade):
+    numero=int(input("Digite um número: "))
+    soma+
+
